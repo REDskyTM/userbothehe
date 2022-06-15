015253a39bc8197087c0764b20ef961f86be9810
REDskyTM <71713652+REDskyTM@users.noreply.github.com>
2022-06-15 13:19:08 +0600

    Create uptime.py

---
+++ plugins/uptime.py
@@ -0,0 +1,21 @@
+import os
+from pyrogram import Client, filters, types
+from m2h import Sec2Hum
+
+
+class Help:
+    des = 'Показывает время работы бота'
+    ver = '2.0'
+    in_help = "upt"
+    author = "Chaos, Koban"
+    cmd_list = {
+        'upt': 'узнать время работы бота'
+    }
+
+
+@Client.on_message(filters.me & filters.command("upt", Client.prefix))
+async def uptime_handler(_, msg: types.Message):
+    timer = os.popen("cat /proc/uptime").read()
+    timer = float(timer.split()[0])
+    result = Sec2Hum(timer)
+    await msg.edit_text(f"Бот работает: <b><i>{result}</i></b>")
