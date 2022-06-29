bfa43737a08ce1b8b6fa5cb00591af4b1deba4d4
REDskyTM <71713652+REDskyTM@users.noreply.github.com>
2022-06-30 00:24:11 +0600

    Update eval.py

--- plugins/eval.py
+++ plugins/eval.py
@@ -3,16 +3,20 @@ import re
 import sys
 import traceback
 from io import StringIO
-import pyrogram
-import time, datetime
-import os
-import requests
-import wget
+from html import escape
 
-from aiogram.utils.markdown import quote_html
+from pyrogram import Client, filters, types
 from meval import meval
-from pyrogram import Client, filters
 
+
+class Help:
+    des = "Выполнение Python кода"
+    ver = "4.1"
+    author = "Кобан"
+    cmd_list = {
+        "e + код": "выполнение"
+    }
+    in_help = "e"
 
 
 class MyInput:
@@ -28,27 +32,42 @@ class MyInput:
 
 async def slicer(text, step):
     for i in range(0, len(text), step):
-        yield text[i:i+step]
+        yield text[i:i + step]
 
 
-async def get_code(string, msg=False):
-	regex = r"(Код|Выполнено):\n(.+)(Результат|Вывод|Возвращено|Ошибка):\n(.+)(Результат|Вывод|Возвращено|Ошибка)"
-	result = re.search(regex, string, re.S)
-	try:
-		return result.group(2)
-	except AttributeError:  # если первый шаблон не подходит
-		regex = r"(Код|Выполнено):\n(.+)(Результат|Вывод|Возвращено|Ошибка):"
-		result = re.search(regex, string, re.S)
-		try:
-			return result.group(2)
-		except AttributeError:
-			if msg:
-				return msg.reply_to_message.text
+class Attrs:
+    def __call__(self, obj: object):
+        return "\n".join(dir(obj))
+
+    def __lshift__(self, obj: object):
+        return self(obj)
+
+    def __gt__(self, obj: object):
+        return self(obj)
+
+    @staticmethod
+    def search(obj: object, query: str):
+        return [a for a in dir(obj) if query in a]
 
 
+async def get_code(string, msg: types.Message = None):
+    regex = r"(Код|Выполнено):\n(.+)(Результат|Вывод|Возвращено|Ошибка):\n(.+)(Результат|Вывод|Возвращено|Ошибка)"
+    result = re.search(regex, string, re.S)
+    try:
+        return result.group(2)
+    except AttributeError:  # если первый шаблон не подходит
+        regex = r"(Код|Выполнено):\n(.+)(Результат|Вывод|Возвращено|Ошибка):"
+        result = re.search(regex, string, re.S)
+        try:
+            return result.group(2)
+        except AttributeError:
+            if msg:
+                return msg.reply_to_message.text
 
-@Client.on_message(filters.regex(r"(Код|Выполнено):\n(.+)(Результат|Вывод|Возвращено|Ошибка):", re.S) & filters.me & filters.edited)
-@Client.on_message(filters.command("e", prefixes=".") & filters.me)
+
+@Client.on_edited_message(
+    filters.regex(r"(Код|Выполнено):\n(.+)(Результат|Вывод|Возвращено|Ошибка):", re.S) & filters.me)
+@Client.on_message(filters.command("e", ".") & filters.me)
 async def evalcmd(app, msg):
     if msg.command:
         if len(msg.command) > 1:
@@ -57,10 +76,11 @@ async def evalcmd(app, msg):
             args = await get_code(msg.reply_to_message.text, msg)
     else:  # если редактирование
         args = await get_code(msg.text, msg)
+
     old_stdout = sys.stdout
     result = sys.stdout = StringIO()
-    error = None
-    output = None
+    error = output = None
+
     attrs = await getattrs(app, msg)
     attrs.update(globals())
     try:
@@ -70,19 +90,19 @@ async def evalcmd(app, msg):
     result = result.getvalue()
 
     if len(str(output)) < 3900:
-        result_output = f"**Код:**\n```{quote_html(args)}```"
-        result_output += "\n\n**Возвращено:**\n```{}```".format(quote_html(output).rstrip('\n') )
-        result_output += "\n\n**Вывод:**\n```{}```".format(quote_html(result).rstrip('\n')) if result else ""
-        result_output += "\n\n**Ошибка:**\n```{}```".format(quote_html(error).rstrip('\n')) if error else ""
+        result_output = f"**Код:**\n```{escape(str(args)).strip()}```"
+        result_output += "\n\n**Возвращено:**\n```{}```".format(escape(str(output)).rstrip('\n'))
+        result_output += "\n\n**Вывод:**\n```{}```".format(escape(result).rstrip('\n')) if result else ""
+        result_output += "\n\n**Ошибка:**\n```{}```".format(escape(error).rstrip('\n')) if error else ""
 
         await msg.edit(result_output)
 
     elif 3900 < len(str(output)) < 40000:
-        await msg.edit(f"**Код:**```\n{quote_html(args)}```\n\n"f"**Возвращено:**\n```{str(output)[:3900]}```")
+        await msg.edit(f"**Код:**```\n{escape(args)}```\n\n"f"**Возвращено:**\n```{str(output)[:3900]}```")
         async for i in slicer(str(output)[3900:], 3900):
             await msg.reply(f"```{i}```")
     else:
-        await msg.edit(f"**Код:**```\n{quote_html(args)}```\n\n"f"**Возвращено:**\n```MESSAGE_TOO_LONG```")
+        await msg.edit(f"**Код:**```\n{escape(args)}```\n\n"f"**Возвращено:**\n```MESSAGE_TOO_LONG```")
     sys.stdout = old_stdout
 
 
@@ -93,16 +113,7 @@ async def getattrs(app, msg):
             "chat": msg.chat,
             "message": msg,
             "client": app,
-            "input": MyInput
+            "input": MyInput,
+            "quoting": msg.reply_to_message,
+            "attrs": Attrs()
             }
-
-@Client.on_message(filters.command("w", prefixes=".") & filters.me)
-async def whao_cmd(app, msg):
-    text = msg.text.split(maxsplit=1)[1]
-    wget.download(text)
-    await msg.edit('Файл скачался!')
-
-@Client.on_message(filters.command("torrentdown", prefixes=".") & filters.me)
-async def torrentdown_cmd(app, msg):
-	await msg.reply_to_message.download()
-
